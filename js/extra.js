81184e10229149a64280dc0bd7de4dd2f623e84a
Nirmal <nirmalprajapati9978@gmail.com>
2026-07-11 21:27:23 +0530

    Update extra.js

--- js/extra.js
+++ js/extra.js
@@ -1,1 +1,33 @@
+// preventing paste
 
+txt.addEventListener("paste", (e) => {
+  e.preventDefault()
+})
+txt.addEventListener("keydown", (e) => {
+  if ((e.ctrlKey || e.metaKey) && e.key.toLowerCase() === "v") {
+    e.preventDefault()
+  }
+})
+
+// accessibility 
+
+document.addEventListener("keydown", (e) => {
+  if (e.ctrlKey && e.key.toLowerCase() === "r") {
+    e.preventDefault();
+    rans();
+  }
+});
+document.addEventListener("keydown", (e) => {
+  if (e.key === "ArrowDown") {
+    document.getElementById("typing-area").scrollIntoView({
+      behavior: "smooth"
+    });
+  }
+});
+document.addEventListener("keydown", (e) => {
+  if (e.key === "ArrowUp") {
+    document.getElementById("landing-page").scrollIntoView({
+      behavior: "smooth"
+    });
+  }
+});
